fb1baaa39c3b4d00eb62b48a10d564d0a2b629dc
hh2333 <tongsj2333@gmail.com>
2021-10-13 21:22:23 +0530

    onclite: Take log without ADB

--- device.mk
+++ device.mk
@@ -347,7 +347,8 @@ PRODUCT_PACKAGES += \
     init.qcom.usb.rc \
     init.recovery.qcom.rc \
     init.target.rc \
-    ueventd.qcom.rc
+    ueventd.qcom.rc\
+    init.logcat.rc
 
 PRODUCT_PACKAGES += \
     init.class_main.sh \

c4e34622b7ed43ce6038053a3fa430b92f443e7d
bibarub <wi1717171713@gmail.com>
2022-01-01 21:01:22 +0300

    onclite: don't override dalvik heap props
    they're being overriden by libinit
    
    * leave davlik heap config include as fallback, in case if libinit doesn't get loaded for some reason

--- device.mk
+++ device.mk
@@ -131,10 +131,6 @@ PRODUCT_PACKAGES += \
     GCamGOPrebuilt
 
 # Dalvik
-PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.heapminfree=4m \
-    dalvik.vm.heapstartsize=16m
-
 $(call inherit-product, frameworks/native/build/phone-xhdpi-2048-dalvik-heap.mk)
 
 # Component overrides
